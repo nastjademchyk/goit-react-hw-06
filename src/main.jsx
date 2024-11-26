d21af044ecba5e93470804fdc4240b81211c0aa2
Nastja Demchyk <NastjaDemchyk@gmail.com>
2024-11-26 14:46:55 +0100

    completed first settings for homework

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import "modern-normalize";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
